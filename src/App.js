0a28ee5fc23b0fb126b9b68f79caa3ed22f8f6c8
aniigupta <aniket8076206264@gmail.com>
2024-07-24 14:04:57 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Loginsignup from './Pages/Loginsignup';
+import Cart from './Pages/Cart';
+import Shopcategory from './Pages/shopcategory';
+import Shop from './Pages/shop';
+import Product from './Pages/product';
 
 function App() {
   return (
     <div>
-    <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/mens' element={<Shopcategory category="men" />} />
+          <Route path='/womens' element={<Shopcategory category="women" />} />
+          <Route path='/kids' element={<Shopcategory category="kid" />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/:productid' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Loginsignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
